8bb8f5f2bd1173eaa78a4d24c185afcd687a6780
drumttocs8 <drumttocs8@yahoo.com>
2026-01-20 20:01:19 -0500

    added start code

---
+++ main.py
@@ -0,0 +1,8 @@
+git clone https://github.com/GRIDAPPSD/gridappsd-docker
+cd gridappsd-docker
+
+#Run the docker containers
+./run.sh
+
+#Once inside the container start gridappsd
+./run-gridappsd.sh
